a57d9949047ef288787cc61dba7cf2d0fa7a0aa4
Abdiel Janulgue <abdiel.janulgue@gmail.com>
2015-09-30 07:22:18 +0000

    Add version

--- scrape.pl
+++ scrape.pl
@@ -13,6 +13,7 @@ my $feed_raw = get("http://grsecurity.net/testing_rss.php");
 
 my $feed = XMLin($feed_raw, ForceArray => ['item']);
 my $filename;
+my @filenames;
 my $link;
 my $new_patches;
 
@@ -20,7 +21,7 @@ for(@{$feed->{channel}->{item}}) {
 	$link = $_->{link};
 	$filename = basename($link);
 	if ( ! -e $script_dir . "/test/". $filename ) {
-		$new_patches++;
+	        $filenames[$new_patches++] = $filename;
 		print("Downloading ", $filename, " ...\n");
 		getstore($link . ".sig", $script_dir . "/test/" . $filename . ".sig");
 		getstore($link, $script_dir . "/test/" . $filename);
@@ -29,9 +30,14 @@ for(@{$feed->{channel}->{item}}) {
 if ($new_patches) {
 	print("Downloading changelog-test.txt ...\n");
 	getstore("http://grsecurity.net/changelog-test.txt", $script_dir . "/test/changelog-test.txt");
-	
+	my $log;
+	for my $p (@filenames) {
+	    (my $ver = $p) =~ s/\.[^.]+$//;
+	    $log .= $ver . " "
+	}
+
 	system("git", "add", $script_dir . "/test/" . $filename, $script_dir . "/test/changelog-test.txt", $script_dir . "/test/" . $filename . ".sig");
-	system("git", "commit", "-a", "-m", "Auto commit, " . $new_patches . " new patch{es}.");
-	system("git", "push");
+	system("git", "commit", "-a", "-m", "Auto commit, " . $log . ", "  . $new_patches . " new patch{es}.");
+	system("git", "push", "origin", "master");
 }
 
