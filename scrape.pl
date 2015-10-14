71dd78cc49d0c97355e81bc9a62fbb70a0fd6acc
Abdiel Janulgue <abdiel.janulgue@gmail.com>
2015-10-14 10:59:59 +0000

    Fetch pax patches

--- scrape.pl
+++ scrape.pl
@@ -37,7 +37,42 @@ if ($new_patches) {
 	}
 
 	system("git", "add", $script_dir . "/test/" . $filename, $script_dir . "/test/changelog-test.txt", $script_dir . "/test/" . $filename . ".sig");
-	system("git", "commit", "-a", "-m", "Auto commit, " . $log . ", "  . $new_patches . " new patch{es}.");
-	system("git", "push", "origin", "master");
+	system("git", "commit", "test/", "-m", "Auto commit, " . $log . ", "  . $new_patches . " new patch{es}.");
+}
+
+# Fetch pax patches
+my $url = "http://www.grsecurity.net/~paxguy1/";
+my $page = get($url);
+
+my @content = split(/\n/, $page);
+my @links = grep(/<a.*href=.*>/,@content);
+
+my $new_pax_patches;
+my $pax_log;
+
+foreach my $c (@links){
+	$c =~ /<a.*href="([\s\S]+?)".*>/;
+	my $link = $1;
+	$c =~ /<a.*href.*>([\s\S]+?)<\/a>/;
+	my $title = $1;
+	my $file = $script_dir . "/pax/". "$link";
+	if ($link =~ /linux-4./ && (! -e $file)) {
+	    $new_pax_patches++;
+	    getstore("$url" . "$link", $file);
+	    (my $ver = $link) =~ s/\.[^.]+$//;
+	    $pax_log .= $ver . " ";
+	    print("Downloading " . $script_dir . "/pax/". "$link\n");
+
+	    system("git", "add", $script_dir . "/pax/" . "$link")
+	}
+}
+
+if ($new_pax_patches) {
+    system("git", "commit", "pax/", "-m",
+	   "Auto commit, " . $pax_log . ", "  . $new_pax_patches . " new patch{es}.");
+}
+
+if ($new_pax_patches || $new_patches) {
+    system("git", "push", "origin", "master");
 }
 
